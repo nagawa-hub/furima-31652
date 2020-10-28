bce47db38ac031d35e0fe16e75f93ce31ebd3e95
nagawa-hub <nakagawayu2008@gmail.com>
2020-10-28 18:17:47 +0900

    with_optionでバリデーションの記述を修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :nickname, presence: true
+  
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+  end
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
@@ -19,5 +22,5 @@ class User < ApplicationRecord
     validates :last_kana
   end
 
-  validates :birthday, presence: true
+  
 end
