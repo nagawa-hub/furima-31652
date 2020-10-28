b105191bca50846107a0939ab9a21417e0cc2e6a
nagawa-hub <nakagawayu2008@gmail.com>
2020-10-28 16:44:18 +0900

    userモデルの単体テストコード実装

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname    {"abe"}
+    email       {Faker::Internet.free_email}
+    password    {"abc123"}
+    password_confirmation {password}
+    last_name   {"高橋"}
+    first_name  {"純一"}
+    last_kana   {"タカハシ"}
+    first_kana  {"ジュンイチ"}
+    birthday {"1998/10/22"}
+  end
+end
