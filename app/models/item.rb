eeec3c15a228621e287f30c7d22accea86225d49
nagawa-hub <nakagawayu2008@gmail.com>
2020-10-29 16:36:29 +0900

    アクティブハッシュ（プルダウン）の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,11 @@ class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :item_detail
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :item_fee
+  belongs_to_active_hash :item_day
+
   
   with_options presence: true do
     validates :title
@@ -16,7 +21,7 @@ class Item < ApplicationRecord
     validates :price
   end
 
-  with_options numericality: { other_than: 1 }
+  with_options numericality: { other_than: 1 } 
     validates :category_id
     validates :condition_id
     validates :shipping_charge_id
