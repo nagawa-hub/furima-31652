7d5b704b5e546362f09533c93139d01b37c1cf9f
nagawa-hub <nakagawayu2008@gmail.com>
2020-10-29 19:25:11 +0900

    手数料、利益の計算機能を実装

---
+++ app/javascript/item_price.js
@@ -0,0 +1,14 @@
+function calc (){
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    const itemTax = Math.floor(inputValue * 0.1);
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = itemTax
+    const itemProfit = document.getElementById("profit");
+    itemProfit.innerHTML = inputValue - itemTax
+  });
+
+};
+
+window.addEventListener('load',calc)
