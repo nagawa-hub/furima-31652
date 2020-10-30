dd55f48b661f73056f4e8e0720d9ba96ce17ed7b
nagawa-hub <nakagawayu2008@gmail.com>
2020-10-30 15:36:18 +0900

    商品の一覧をトップページに表示
    activehashのクラス名とファイル名を修正。

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,11 +3,11 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :item_detail
-  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :category
+  belongs_to_active_hash :shipping_prefecture
   belongs_to_active_hash :condition
-  belongs_to_active_hash :item_fee
-  belongs_to_active_hash :item_day
+  belongs_to_active_hash :shipping_charge
+  belongs_to_active_hash :shipping_day
 
   with_options presence: true do
     validates :image
