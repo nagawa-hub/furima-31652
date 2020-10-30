787deec479deffc65256c94b28384dfd0ffa08a5
nagawa-hub <nakagawayu2008@gmail.com>
2020-10-30 18:58:39 +0900

    rubocopの実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index,:show]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -18,9 +18,9 @@ class ItemsController < ApplicationController
     end
   end
 
-   def show
+  def show
     @item = Item.find(params[:id])
-   end
+  end
 
   private
 
