d90239973578912aef08351bcf76d70f8f151b28
nagawa-hub <nakagawayu2008@gmail.com>
2020-10-31 13:35:30 +0900

    activehashの導入

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,2 +1,13 @@
 class OrderAddress < ApplicationRecord
+  belongs_to :order
+
+  with_options presence:true do
+    validates :post_number
+    validates :home_prefecture_id
+    validates :home_town
+    validates :home_address
+    validates :phone_number
+  end
+
+  validates :home_prefecture_id, numericality: { other_than: 1 }
 end
---
+++ spec/factories/home_prefectures.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :home_prefecture do
+    
+  end
+end
