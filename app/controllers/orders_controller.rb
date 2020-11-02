8f4c578a1669f821a9c1d585f44512560a816335
nagawa-hub <nakagawayu2008@gmail.com>
2020-11-02 11:35:26 +0900

    コードの記述を簡略化
    ・アクティブレコードからアソシエーションに変更。
    ・バリデーションをまとめる

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,17 +1,15 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_item
 
   def index
-    @item = Item.find(params[:item_id])
     @item_order = ItemOrder.new
-    if Order.where(item_id: @item.id).exists? || current_user.id == @item.user_id
-      @items = Item.includes(:user).order('created_at DESC')
-      render template: 'items/index'
+    if @item.order || current_user.id == @item.user_id
+      redirect_to root_path
     end
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @item_order = ItemOrder.new(item_order_params)
     if @item_order.valid?
       pay_item
@@ -23,6 +21,9 @@ class OrdersController < ApplicationController
   end
 
   private
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
 
   def item_order_params
     params.require(:item_order).permit(:post_number, :home_prefecture_id, :home_town, :home_address, :building, :phone_number).merge(user_id: current_user.id, item_id: @item.id, price: @item.price, token: params[:token])
